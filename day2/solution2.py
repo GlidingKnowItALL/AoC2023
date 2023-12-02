9ab9411cf1ced50ecfcc75338d65b4622f85864e
GlidingKnowItALL <ivarnordhoy@gmail.com>
2023-12-02 23:05:42 +0100

    Fixed issue where input would include newline, day 2 part 2 finished.

--- day2/solution2.py
+++ day2/solution2.py
@@ -2,11 +2,13 @@ puzzleinput = open("input.txt", "r")
 
 
 def validate(inp):
+    inp = inp.strip()
     sets = inp.split(": ")[1].split("; ")
     red, green, blue = 0, 0, 0
     for i in range(len(sets)):
-        set = sets[i]
-        values = set.split(", ")
+        currentset = sets[i]
+        values = currentset.split(", ")
+
         for x in range(len(values)):
             value = values[x].split(" ")
             if value[1] == "red":
@@ -15,18 +17,20 @@ def validate(inp):
             elif value[1] == "green":
                 if int(value[0]) > green:
                     green = int(value[0])
-            else:
+            elif value[1] == "blue":
                 if int(value[0]) > blue:
                     blue = int(value[0])
-    print(str(red) + ' * ' + str(green) + ' * ' + str(blue) + ' = ' + str(red * green * blue))
+            print(value)
+    # print(str(red) + ' * ' + str(green) + ' * ' + str(blue) + ' = ' + str(red * green * blue))
     return red * green * blue
+
 
 def solve(inp):
     total = 0
     for i in range(len(inp)):
         total += validate(inp[i])
-        print(i+1)
-        print(total)
+        # print(i+1)
+        # print(total)
 
     return total
 

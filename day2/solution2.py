2e2b4c7b6a8c2d77b1dc9ce5fb865879ceda3e7d
GlidingKnowItALL <ivarnordhoy@gmail.com>
2023-12-02 22:27:12 +0100

    day 2 part 2 progress

---
+++ day2/solution2.py
@@ -0,0 +1,36 @@
+puzzleinput = open("input.txt", "r")
+
+
+def validate(inp):
+    sets = inp.split(": ")[1].split("; ")
+    red, green, blue = 0, 0, 0
+    for i in range(len(sets)):
+        set = sets[i]
+        values = set.split(", ")
+        for x in range(len(values)):
+            value = values[x].split(" ")
+            if value[1] == "red":
+                if int(value[0]) > red:
+                    red = int(value[0])
+            elif value[1] == "green":
+                if int(value[0]) > green:
+                    green = int(value[0])
+            else:
+                if int(value[0]) > blue:
+                    blue = int(value[0])
+    print(str(red) + ' * ' + str(green) + ' * ' + str(blue) + ' = ' + str(red * green * blue))
+    return red * green * blue
+
+def solve(inp):
+    total = 0
+    for i in range(len(inp)):
+        total += validate(inp[i])
+        print(i+1)
+        print(total)
+
+    return total
+
+
+print(solve(puzzleinput.readlines()))
+
+
